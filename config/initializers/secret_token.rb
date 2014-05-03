0ae8e8d8b77c57239d8ee28aa1924ed074284044
Jerrold Thompson <jet@whidbey.com>
2014-05-03 10:25:37 -0700

    Base Spree 2-2-stable installed and functioning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fedtax::Application.config.secret_key_base = '0a783295d72916768709de974bbea78b7d20af06e17cc495ba51d90507bea4426f723dc3fa97eef2e6fff1ac8ef42a7c22c00cdc0d802f3ad26f5dce0b932e6c'
